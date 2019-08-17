8ab5ebf630c5a994db9ef225c605ff7bbb0a88d0
Ian Griswold <igriswol@andrew.cmu.edu>
2019-08-17 13:44:31 -0400

    Parsed BPM and STOPS tags, separated notes into measures

--- smparser.py
+++ smparser.py
@@ -28,5 +28,20 @@ def parse(path):
     return tagDict #effectively a song, with multiple charts
 
 class Chart:
-    def __init__(tagDict, ind): #takes an ouput from parse() and an index of which chart to read, copies relevant details
-        pass
+    def __init__(self, tagDict, ind): #takes an ouput from parse() and an index of which chart to read, copies relevant details
+        chart = tagDict["NOTES"][ind] #get the chart we want
+        chart = list(map(lambda x: x.strip(), chart.split(":"))) #split into properties
+        self.notes = list(map(lambda x: x.strip().split("\n"), chart[-1].split(","))) #take just the notes, split into measures
+
+        self.name = tagDict["TITLE"]
+        self.artist = tagDict["ARTIST"]
+
+        bpms = list(map(lambda x: x.strip(), tagDict["BPMS"].split(","))) #split into individual bpm changes
+        self.bpms = list(map(lambda x: tuple(map(lambda y: float(y),x.split("="))), bpms)) #convert to (beat, BPM) pairs of floats
+
+        #same but stops
+        stops = list(map(lambda x: x.strip(), tagDict["STOPS"].split(","))) 
+        if stops == [""]: #what if no stops?
+            self.stops = [];
+        else:
+            self.stops = list(map(lambda x: tuple(map(lambda y: float(y),x.split("="))), stops))
