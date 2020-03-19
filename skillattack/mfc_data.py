be03a3976165a7672eeb725c863e8c84aae2387e
LaplaceFox <mbptemp555@gmail.com>
2020-03-19 01:52:50 -0400

    MFC difficulty metric file output

--- skillattack/mfc_data.py
+++ skillattack/mfc_data.py
@@ -12,7 +12,7 @@ Chart:
 
 #1 point viable charts: 8-10 Difficulty, DSP, ESP, CSP only
 
-def getviable():
+def getViable():
     viables = []
 
     f = open("master_music.txt")
@@ -40,6 +40,71 @@ def breakdown(chart): #modify later to take score cutoffs and labels as input
             sdp += 1
 
     #debug
-    print("MFC: %s\n5LP: %s\nSDP: %s\nTotal: %s"%(mfc,flp,sdp,len(scorelist)))
+    #print("MFC: %s\n5LP: %s\nSDP: %s\nTotal: %s"%(mfc,flp,sdp,len(scorelist)))
 
     return [mfc, flp, sdp, len(scorelist)]
+
+def cmetrics(brkdwn, a, b, succession = False):
+    #if succession == True, we use Laplace's Rule of Succession (no relation)
+    mfc   = brkdwn[0]
+    flp   = brkdwn[1]
+    sdp   = brkdwn[2]
+    total = brkdwn[3]
+
+    if succession:
+        mfc += 1
+        flp += 2
+        sdp += 3
+        total += 4
+
+    return (mfc + a*flp + b*sdp) / total
+
+def precomputeBreakdowns():
+    viablelist = getViable()
+    f = open("1point-viable-breakdowns.txt","w")
+    f.write("chartID;MFC;5LP;SDP;Total\n")
+
+    for i in range(len(viablelist)):
+        print("[%s/%s]"%(i+1,len(viablelist)))
+        chart = viablelist[i]
+        brks = list(map(lambda x: str(x), breakdown(chart)))
+        f.write(chart.id + ";" + ";".join(brks) + "\n")
+    f.close()
+
+def computeMetrics(a, b, succession = False):
+    viablelist = getViable()
+    chartDict = {}
+    for chart in viablelist:
+        chartDict[chart.id] = chart
+
+    f = open("1point-viable-breakdowns.txt")
+    lines = f.readlines()[1:]
+    f.close()
+
+    metriclist = [] #list of pairs of (metric, ID)
+
+    for line in lines:
+        line = line.split(";")
+        chartID = line[0]
+        brkdwn = list(map(lambda x: int(x), line[1:])) #remove the id, convert to int
+        metric = cmetrics(brkdwn, a, b, succession)
+
+        metriclist.append((metric, chartID))
+
+    metriclist.sort()
+    metriclist = metriclist[::-1] #sort from largest to smallest
+
+    f = open("metric_out.txt","w")
+
+    f.write("Metric used: (#MFC + %s*#L5P + %s*#SDP) / #scores\n"%(a,b))
+    nextline = "Laplace's rule of succession used: "
+
+    if succession:
+        f.write(nextline + "Yes\n\n")
+    else:
+        f.write(nextline + "No\n\n")
+
+    for (metric, chartID) in metriclist:
+        title = chartDict[chartID].name
+        f.write("%s : %.5f\n"%(title,metric))
+    f.close()
